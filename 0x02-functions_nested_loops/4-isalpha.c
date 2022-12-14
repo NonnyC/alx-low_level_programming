21812e4933044d5d34caa1f3b68a642a4f9f368d
NonnyC <chinonsonwoji6693@gmail.com>
2022-12-14 01:23:15 -0800

    Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,10 @@
 /**
  * _isalpha -checks for alphabetic character
  * @c: the character to checked
- * Return:1 if c is lowercase or uppercase, 0 otherwise
+ * Return:1 if c is letter, 0 otherwise
  */
 
-int _isalpha(int c
+int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
