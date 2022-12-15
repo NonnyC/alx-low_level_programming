bd5fa93248c558bb0b644feb9bd4e9a6835b5258
NonnyC <chinonsonwoji6693@gmail.com>
2022-12-15 08:43:07 -0800

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,25 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i == 1)
-{
+i = 1;
 printf("%d", i);
+for (i = 2; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
 }
 else
 {
-printf(" %d", i);
+printf("%d", i);
 }
 }
 printf("\n");
